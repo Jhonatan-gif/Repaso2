32cec93c012544742e0db300807f6bca7b6cc596
Jhonatan Tipan <jhonatan.tipan@udla.edu.ec>
2025-04-29 03:35:55 -0500

    Agregar archivos de proyecto.

---
+++ Repaso2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Repaso2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
